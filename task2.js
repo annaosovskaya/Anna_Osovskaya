0de14876391dc72e0606c8de11d4f6d34797bf45
annaosovskaya <98734679+annaosovskaya@users.noreply.github.com>
2023-03-31 21:26:56 +0300

    Delete task2.js

--- task2.js
+++
@@ -1,9 +0,0 @@
-function firstNotRepeatingCharacter(s) {
-    let strArr = s.replace(" ", "").split("");
-    for (let str of strArr) 
-        if(strArr.indexOf(str) == strArr.lastIndexOf(str))
-            return str;
-        
-    return "_";
-};
-console.log(firstNotRepeatingCharacter("abacabad"));
